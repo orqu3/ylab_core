a7b3045f25bd6dd19ec2a7a5a5e4887644007b1d
Mariia Shchukina <shchuk1naaams@gmail.com>
2022-08-29 15:01:18 +0400

    task_2

---
+++ src/task_2/Main.java
@@ -0,0 +1,41 @@
+package task_2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        var array = new int[]{5, 6, 3, 2, 5, 1, 4, 9};
+        quickSort(array, 0, array.length - 1);
+
+        assert Arrays.equals(array, new int[]{1, 2, 3, 4, 5, 5, 6, 9});
+    }
+
+    public static void quickSort(int[] array, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+
+            quickSort(array, begin, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] array, int begin, int end) {
+        int pivot = array[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+
+                int swapTemp = array[i];
+                array[i] = array[j];
+                array[j] = swapTemp;
+            }
+        }
+        int swapTemp = array[i + 1];
+        array[i + 1] = array[end];
+        array[end] = swapTemp;
+
+        return i + 1;
+    }
+}
