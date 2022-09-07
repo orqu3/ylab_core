bade048db9b9944c3c06ecf0ff7503d94abacbf9
Mariia Shchukina <shchuk1naaams@gmail.com>
2022-09-07 15:48:51 +0400

    task 3

--- src/homework_2/task_3/Main.java
+++ src/homework_2/task_3/Main.java
@@ -12,4 +12,35 @@ package homework_2.task_3;
               fuzzySearch("lw", "cartwheel"); // false
    */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(fuzzySearch("car", "ca6$$#_rtwheel"));
+        System.out.println(fuzzySearch("cwhl", "cartwheel"));
+        System.out.println(fuzzySearch("cwhee", "cartwheel"));
+        System.out.println(fuzzySearch("cartwheel", "cartwheel"));
+        System.out.println(fuzzySearch("cwheeel", "cartwheel"));
+        System.out.println(fuzzySearch("lw", "cartwheel"));
+    }
+
+    private static boolean fuzzySearch(String target, String original) {
+        if (original.length() < target.length() ||
+                original.length() == target.length()
+                        && original.charAt(0) != target.charAt(0)) {
+            return false;
+        }
+        int i = 0;
+        for (int j = 0; j < original.length(); ++j) {
+            char targetChar = target.charAt(i);
+            char originalChar = original.charAt(j);
+            if (targetChar == originalChar) {
+                ++i;
+                if (i == target.length()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
+
